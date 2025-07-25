bf429303911ac703d99d7e692a3b2277cff1d2db
Sujay0610 <117998140+Sujay0610@users.noreply.github.com>
2025-07-25 13:22:34 +0530

    Update LoginPage.tsx

--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -102,7 +102,7 @@ const LoginPage: React.FC = () => {
       const response = await apiService.createUser({
         full_name: createUserForm.fullName,
         email: createUserForm.email,
-        phone: createUserForm.phone,
+        phone?: createUserForm.phone,
         address: createUserForm.address
       });
       
